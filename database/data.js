17fbdd0045a72b4293ea0794349776a1d376ec34
Jansan-Jathushan <jansanjathushan@gmail.com>
2024-12-01 02:03:39 +0530

    first commit

--- database/data.js
+++ database/data.js
@@ -1,8 +1,58 @@
+// export default [
+//     {
+//         id: 1,
+//         question : "Javascript is an _______ language",
+//         options : [
+//             'Object-Oriented',
+//             'Object-Based',
+//             'Procedural',
+//         ]
+//     },
+//     {
+//         id: 2,
+//         question : "Following methods can be used to display data in some form using Javascript",
+//         options : [
+//             'document.write()',
+//             'console.log()',
+//             'window.alert()',
+//         ]
+//     },
+//     {
+//         id: 3,
+//         question : "When an operator value is NULL, the typeof returned by the unary operator is:",
+//         options : [
+//             'Boolean',
+//             'Undefined',
+//             'Object',
+//         ]
+//     },
+//     {
+//         id: 4,
+//         question : "What does the toString() method return?",
+//         options : [
+//             'Return Object',
+//             'Return String',
+//             'Return Integer'
+//         ]
+//     },
+//     {
+//         id: 5,
+//         question : "Which function is used to serialize an object into a JSON string?",
+//         options : [
+//             'stringify()',
+//             'parse()',
+//             'convert()',
+//         ]
+//     }
+// ];
+
+// export const answers = [0, 1, 2, 1, 0];
+
 export default [
     {
         id: 1,
-        question : "Javascript is an _______ language",
-        options : [
+        question: "Javascript is an _______ language",
+        options: [
             'Object-Oriented',
             'Object-Based',
             'Procedural',
@@ -10,8 +60,8 @@ export default [
     },
     {
         id: 2,
-        question : "Following methods can be used to display data in some form using Javascript",
-        options : [
+        question: "Following methods can be used to display data in some form using Javascript",
+        options: [
             'document.write()',
             'console.log()',
             'window.alert()',
@@ -19,8 +69,8 @@ export default [
     },
     {
         id: 3,
-        question : "When an operator value is NULL, the typeof returned by the unary operator is:",
-        options : [
+        question: "When an operator value is NULL, the typeof returned by the unary operator is:",
+        options: [
             'Boolean',
             'Undefined',
             'Object',
@@ -28,8 +78,8 @@ export default [
     },
     {
         id: 4,
-        question : "What does the toString() method return?",
-        options : [
+        question: "What does the toString() method return?",
+        options: [
             'Return Object',
             'Return String',
             'Return Integer'
@@ -37,13 +87,103 @@ export default [
     },
     {
         id: 5,
-        question : "Which function is used to serialize an object into a JSON string?",
-        options : [
+        question: "Which function is used to serialize an object into a JSON string?",
+        options: [
             'stringify()',
             'parse()',
             'convert()',
         ]
+    },
+    {
+        id: 6,
+        question: "Which company developed JavaScript?",
+        options: [
+            'Netscape',
+            'Microsoft',
+            'Google',
+        ]
+    },
+    {
+        id: 7,
+        question: "Which of the following is not a JavaScript data type?",
+        options: [
+            'Number',
+            'String',
+            'Float',
+        ]
+    },
+    {
+        id: 8,
+        question: "Which symbol is used for comments in JavaScript?",
+        options: [
+            '//',
+            '/* */',
+            '#',
+        ]
+    },
+    {
+        id: 9,
+        question: "What is the default value of an uninitialized variable in JavaScript?",
+        options: [
+            'null',
+            'undefined',
+            '0',
+        ]
+    },
+    {
+        id: 10,
+        question: "Which JavaScript keyword is used to declare variables?",
+        options: [
+            'var',
+            'let',
+            'Both var and let',
+        ]
+    },
+    {
+        id: 11,
+        question: "Which method is used to find HTML elements by ID in JavaScript?",
+        options: [
+            'getElementById()',
+            'querySelector()',
+            'getElementsByClassName()',
+        ]
+    },
+    {
+        id: 12,
+        question: "Which JavaScript method is used to add new elements to an array?",
+        options: [
+            'push()',
+            'concat()',
+            'splice()',
+        ]
+    },
+    {
+        id: 13,
+        question: "What is the purpose of the isNaN() function in JavaScript?",
+        options: [
+            'To check if a value is null',
+            'To check if a value is a number',
+            'To check if a value is NaN',
+        ]
+    },
+    {
+        id: 14,
+        question: "Which statement is used to stop a loop in JavaScript?",
+        options: [
+            'exit',
+            'break',
+            'stop',
+        ]
+    },
+    {
+        id: 15,
+        question: "Which JavaScript framework is maintained by Facebook?",
+        options: [
+            'Angular',
+            'React',
+            'Vue',
+        ]
     }
 ];
 
-export const answers = [0, 1, 2, 1, 0];
+export const answers = [0, 1, 2, 1, 0, 0, 2, 0, 1, 2, 0, 0, 2, 1, 1];
